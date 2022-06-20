88070a595cce2e57ce39780cb394215a983c95fc
0G3NS3C <40295857+0G3NS3C@users.noreply.github.com>
2022-06-20 22:14:08 +0200

    save

--- api/_collections/Profile/middlewares/profile.middleware.attach.js
+++ api/_collections/Profile/middlewares/profile.middleware.attach.js
@@ -1,9 +1,7 @@
 
 
 module.exports =  async function middleware_checkAccess({ req, res, next }) {
-    console.log('middleware attach profil');
     if (req.user) {
-            console.log('profil found and attach');
         req.profile = await node.collections.profile.manager.findByUser(req.user);
     }
     next();
